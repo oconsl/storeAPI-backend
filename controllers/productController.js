dd22362352411b292bda5a55d67368a43f644579
Santiago Ocon <ocon.santiago@gmail.com>
2022-05-27 11:46:37 -0300

    productController done.

---
+++ controllers/productController.js
@@ -0,0 +1,52 @@
+const productController = (Product) => {
+  // GET all Products
+  const getProducts = async (_, res, next) => {
+    try {
+      const products = await Product.find({})
+
+      res.status(200).json(products)
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  // POST Product
+  const postProduct = async (req, res, next) => {
+    try {
+      const { body } = req
+      const product = new Product(body)
+
+      await product.save()
+
+      res.status(201).json(product)
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  // PUT Product by ID
+  const putProduct = async (req, res, next) => {
+    try {
+      const { params, body } = req
+      const product = await Product.findByIdAndUpdate(params.id, body, {
+        new: true
+      })
+
+      res.status(200).json(product)
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  // DELETE Product by ID
+  const deleteProduct = async (req, res, next) => {
+    try {
+      const { params } = req
+      const product = await Product.findByIdAndDelete(params.id)
+
+      res.status(200).json(product)
+    } catch (err) {
+      next(err)
+    }
+  }
+}
---
+++ routes/productRouter.js
@@ -0,0 +1,2 @@
+import express from 'express'
+import productController from '../controllers/productController.js'
